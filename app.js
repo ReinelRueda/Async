1da32eaf806d7a9614447e2bb3c989578709ce4c
ReinleRueda <reinelr13@gmail.com>
2021-08-25 21:15:08 -0500

    Pintando los datos del Api en el Dom

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const getNombreAsync = async (idPost) => {
+ const HTMLResponse = document.querySelector("#app")
+ const ul = document.createElement('ul')
+
+
+ const getNombreAsync = async () => {
 
 try {
-    const resPost = await fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+    const resPost = await fetch(`http://localhost:3000/api`)
     const post = await resPost.json()
-   
-    const resUser = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
-    const user = await resUser.json()
-    console.log(user.name)
-    
+
+    post.forEach(element => {
+    let elem = document.createElement('li')
+    elem.appendChild(document.createTextNode(`${element.nombre} ---- ${element.logo}`))   
+    ul.appendChild(elem)     
+    });
+    HTMLResponse.appendChild(ul)
+    // console.log(post)
+    // const tpl = post.map(post => `<li>${post.nombre} ---- ${post.logo}</li>`)
+    // HTMLResponse.innerHTML = `<ul>${tpl}</ul>`
+
 } catch (error) {
     console.log(error)
 }
 }
-getNombreAsync(80)
+getNombreAsync()
